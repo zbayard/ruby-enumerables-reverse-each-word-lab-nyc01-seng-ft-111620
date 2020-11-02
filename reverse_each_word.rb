c84e4132eaf5ece7f429a036336861715fa1e281
Zach Bayard <zbayard@gmail.com>
2020-11-02 16:24:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
-def reverse_each_word("Hello there, and how are you?")
-  str.split(" ")
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
